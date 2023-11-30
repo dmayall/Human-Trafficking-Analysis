33e1737ff36f8f7aa0b7a336894b114ef77dcba9
dmayall <derrickmayall@yahoo.com>
2023-11-29 22:49:44 -0500

    Added geographical graph

--- Project.py
+++ Project.py
@@ -3,20 +3,22 @@ import altair as alt
 import streamlit as st
 from vega_datasets import data
 
-
+alt.data_transformers.disable_max_rows()
 @st.cache_data
 def readData():
     data = pd.read_csv('Trafficking_Data.csv')
     coordinates = pd.read_csv('countries_codes_and_coordinates.csv')
-    coordinates = coordinates[['Country', 'Alpha-3 code', 'Latitude (average)', 'Longitude (average)']]
+    coordinates = coordinates[['Country', 'Alpha-3 code', 'Latitude (average)', 'Longitude (average)', 'Numeric code']]
     coordinates['Alpha-3 code'] = coordinates['Alpha-3 code'].str.replace('"','')
     coordinates['Longitude (average)'] = coordinates['Longitude (average)'].str.replace('"','').astype(float)
     coordinates['Latitude (average)'] = coordinates['Latitude (average)'].str.replace('"','').astype(float)
+    coordinates['Numeric code'] = coordinates['Numeric code'].str.replace('"','').astype(int)
+    
+
     return data, coordinates
 trafficking, coordinates = readData()
 st.title('Human Trafficking Analysis')
 st.write('This Dashboard is to show the different types of trafficking and show the patterns of what people are being trafficked for in different parts of the world.')
-st.write(coordinates)
 #Some prprocessing of the data so that it is easily graphed
 trafficking = trafficking.fillna(0)
 #I chose to fill with 0's because of the binary part of the conditions of the trafficking
@@ -27,42 +29,47 @@ trafficking['CountryOfExploitation'] = trafficking['CountryOfExploitation'].repl
 trafficking['traffickMonths'] = trafficking['traffickMonths'].replace(0,'NA') 
 #merging to get coordinate data for my map with my coordinate data
 coordinates['Alpha-3 code'] = coordinates['Alpha-3 code'].str.strip()
+coordinates['Country'] = coordinates['Country'].str.strip()
 trafficking['CountryOfExploitation'] = trafficking['CountryOfExploitation'].str.strip()
 trafficking = pd.merge(trafficking, coordinates, how='left', left_on='CountryOfExploitation',right_on='Alpha-3 code')
 
-'''
-Graphing Ideas:
--Map showing density of country of exploitation
--Show the reason for trafficking and sex of those being trafficked
--Show age ranges being trafficked to look for patterns
-Filter ideas
--Filter by country of citizenship that you are interested in
--add an optional filter for year
--optional filter for the types of trafficking 
-'''
+_='''
+    Graphing Ideas:
+    -Map showing density of country of exploitation
+    -Show the reason for trafficking and sex of those being trafficked
+    -Show age ranges being trafficked to look for patterns
+    Filter ideas
+    -Filter by country of citizenship that you are interested in
+    -add an optional filter for year
+    -optional filter for the types of trafficking 
+    '''
 
 #Starting on making a map
 #_______________________________________________________________
 countries = alt.topo_feature(data.world_110m.url, 'countries')
 #Setting the background of all the countries
-background = alt.Chart(countries).mark_geoshape(
-    fill='lightgray',
-    stroke='white'
-).project(
-    "equirectangular"
-).properties(
-    width=500,
-    height=300
-)
+# background = alt.Chart(countries).mark_geoshape(
+#     fill='lightgray',
+#     stroke='white'
+# ).project(
+#     "equirectangular"
+# ).properties(
+#     width=500,
+#     height=300
+# )
 #Getting data for the points
 graph_data = trafficking.groupby('Alpha-3 code').size().reset_index(name='count')
 graph_data = pd.merge(graph_data, trafficking, how='left', on='Alpha-3 code')
-points = alt.Chart(graph_data).mark_circle(opacity=.5).encode(
-    longitude='Longitude (average)',
-    latitude='Latitude (average)',
-    size='count',
-    tooltip=['Country', 'count']
-
+plot = alt.Chart(countries).mark_geoshape().encode(
+    color='count:Q',
+).transform_lookup(
+    lookup='id',
+    from_=alt.LookupData(graph_data, key='Numeric code', fields=['Country', 'count'])
+).project('equirectangular')\
+.properties(
+    width=500,
+    height=300,
+    title='Denstiy Of Trafficking Destinations'
 )
-plot = background + points
+
 st.altair_chart(plot, use_container_width=True)
